bbf0c07dcbbd7142faeee50c136c2d43525f2846
John DiNofrio <johnjdino@gmail.com>
2020-03-17 15:36:07 -0700

    refactored the code

--- Astar.py
+++ Astar.py
@@ -3,7 +3,7 @@ import cv2
 import math
 import obstacle_map
 import time
-from queue import PriorityQueue
+from Queue import PriorityQueue
 
 def load_map(fname = None):
 	if fname is not None :
@@ -41,6 +41,10 @@ def detectCollision(img, center,radius):
 	return False
 
 
+def euclideanDistance(state1, state2):
+	return np.sqrt(((state1[0] - state2[0]) ** 2) + ((state1[1] - state2[1]) ** 2))
+
+
 def getStartNode(map_):
 	print("Enter the start co-ordinates")
 	rows, cols= map_.shape[:2]
@@ -48,9 +52,10 @@ def getStartNode(map_):
 		## Cartesian Form
 		x = int(input("x_intial is: "))
 		y = int(input("y_intial is: "))
+		theta = int(input("theta_intial is (in degree): "))/30
 		## image coordinates
 		row = rows-y-1 ; col = x
-		if not isValidNode(map_, row, col):
+		if not isValidNode(map_, row, col,0):
 			print('Input Node not within available map range. Please enter again!')
 		else:
 			break;
@@ -65,7 +70,7 @@ def getGoalNode(map_):
 		y = int(input("y_goal is: "))
 		## image coordinates
 		row = rows-y-1 ; col = x
-		if not isValidNode(map_, row, col):
+		if not isValidNode(map_, row, col,0):
 			print('Input Node not within available map range. Please enter again! ')
 		else:
 			break;
@@ -77,32 +82,26 @@ class Node:
 	# visited : flag to identify visited nodes
 	# parent : coordinate location of the parent
 	# cost : cost of reaching current node from start node
-	def __init__(self, visited=False,parent=None, cost = math.inf ):
+	def __init__(self, visited=False,parent=None, cost = float('inf') ):
 		self.visited = visited
 		self.parent = parent
 		self.cost = cost
 
-def updateNeighbours(arr, map_, curr_node,queue,step_size,theta_size,visited):
+def updateNeighbours(arr, map_, curr_node,queue,visited,goal_node,step_size=2,theta_size=30):
 	x,y,theta = curr_node
 	threshold = 0.5
 	for angle in range(-2, 2,1):
 		theta_new = (theta+angle)*theta_size
 		x_new = x + step_size*math.cos(theta_new)
+		x_new = int(round(x_new)/0.5)
 		y_new = y + step_size*math.sin(theta_new)
+		y_new = int(round(y_new)/0.5)
 		theta_new = theta_new/theta_size
-		if isValidNode(map_,x_new,y_new,theta_new) and not visited[int(round(x_new)/threshold)][int(round(y_new)/threshold)][theta_new]:
-			queue.put((arr[x_new][y_new][theta_new].cost,(x_new,y_new,theta_new)
+		if isValidNode(map_,x_new,y_new,theta_new,0) and not visited[x_new][y_new][theta_new]:
+			arr[x_new][y_new] = arr[x][y].cost + euclideanDistance((x_new,y_new),goal_node)
+			queue.put((arr[x_new][y_new].cost,(x_new,y_new,theta_new)))
+			arr[x_new][y_new].parent = (x,y)
 
-
-def findMinCost(arr):
-	curr_node = (-1,-1)
-	min_cost = math.inf
-	for row in range(len(arr)):
-		for col in range(len(arr[row])):
-			if arr[row][col].cost < min_cost and not arr[row][col].visited:
-				curr_node = (row,col)
-				min_cost = arr[row][col].cost
-	return curr_node
 
 def tracePath(arr,map_,goal_node):
 	images= []
@@ -137,14 +136,16 @@ def exploredPath(map_,arr):
 				img[row][col]==(0,255,255)
 	return img
 
+
 def main():
 	map_ = load_map()
 	start_node = getStartNode(map_)
 	goal_node = getGoalNode(map_)
 	rows, cols = map_.shape[:2]
 	depth = 12
-	arr = np.array([[[Node() for k in range(depth)]for j in range(cols)] for i in range(rows)])
-	arr[start_node[0]][start_node[1]].visited = True
+	arr = np.array([[Node()for j in range(int(cols/0.5))] for i in range(int(rows/0.5))])
+	visited = np.zeros((600,400,12))
+	visited[start_node[0]][start_node[1]][start_node[2]] = True
 	arr[start_node[0]][start_node[1]].cost = 0
 	queue = PriorityQueue()
 	queue.put((arr[start_node[0]][start_node[1]].cost, start_node))
@@ -154,13 +155,13 @@ def main():
 	img[goal_node[0]][goal_node[1]] = (0,0,255)
 	while queue:
 		curr_node = queue.get()[1]
-		if (curr_node == goal_node):
+		if (curr_node[:2] == goal_node[:2]):
 			algo_time = time.time()
 			print('found Goal in {}s at cost {}!!'.format(algo_time-start_time, arr[goal_node[0]][goal_node[1]].cost))
 			tracePath(arr,map_,goal_node)
 			break
 		arr[curr_node[0]][curr_node[1]].visited = True
-		arr = updateNeighbours(arr, map_, curr_node,queue)
+		arr = updateNeighbours(arr, map_, curr_node,queue,visited = visited,goal_node=goal_node)
 		img[curr_node[0]][curr_node[1]] = (0,255,0)
 		#exploredList.append(img)
 		cv2.imshow('explored',img)
