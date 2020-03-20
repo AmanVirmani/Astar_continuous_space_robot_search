fixed degrees and radians

main.py
@@ -1,30 +1,34 @@
 import obstacle_map
-import matplotlib.pyplot as plt
 import numpy as np
 import time
 import math
 import cv2
 from queue import PriorityQueue
 
-
+#create the map
 def load_map(fname = None):
     if fname is not None :
         map_ = cv2.imread(fname)
         return map_
+    #makes the 300x200 pixel world
     world= 255*np.ones((200,300,3))
+    #create the obstacles
     obstacle_map.obstacle_circle(world)
     obstacle_map.obstacle_ellipse(world)
     obstacle_map.obstacle_rhombus(world)
     obstacle_map.obstacle_rectangle(world)
     obstacle_map.obstacle_polygon(world)
-
+    #save the image of the world
     cv2.imwrite('./outputs/map.jpg',world)
     return world
 
-
+#check if a node is valid
 def isValidNode(map_, x, y, theta, r=0):
+    #get the width and height of the map
     rows,cols = map_.shape[:2]
+    #make sure coordinates are in the map
     if 0 <= x-r and x+r < rows and 0 <= y-r and y+r < cols:
+        #check if node is touch an obstacle
         if not detectCollision(map_, (x, y), r):
             return True
         else :
@@ -32,7 +36,7 @@ def isValidNode(map_, x, y, theta, r=0):
     else:
         return False
 
-
+#check if node is touching an obstacle
 def detectCollision(img, center,radius):
     # img = cv2.resize(map_,(int(map_.shape[1]/0.5),int(map_.shape[0]/0.5)))
     # center[0] = int(center[0]/0.5)
@@ -40,25 +44,26 @@ def detectCollision(img, center,radius):
     for i in range(2*radius+1):
         for j in range(2*radius+1):
             if i**2+j**2 <= radius**2:
+                #check if the node is on the obstacle
                 if not ((img[center[0]+i][center[1]+j]==(255,255,255)).all() and (img[center[0]+i][center[1]-j]==(255,255,255)).all()\
                         and (img[center[0]-i][center[1]-j]==(255,255,255)).all() and (img[center[0]-i][center[1]+j]==(255,255,255)).all()):
                     return True
     return False
 
-
+#find the Euclidean Distance from two nodes
 def euclideanDistance(state1, state2):
     return np.sqrt(((state1[0] - state2[0]) ** 2) + ((state1[1] - state2[1]) ** 2))
 
-
+#input start coordinates, theta, radius, and clearance
 def getStartNode(map_):
     print("Enter the start co-ordinates")
     rows, cols= map_.shape[:2]
     while True :
         ## Cartesian Form
-        # x = int(input("x_intial is: "))
-        # y = int(input("y_intial is: "))
-        # theta = int(int(input("theta_intial is (in degree): "))/30)
-        x, y, theta = (0, 0, 2)
+        x = int(input("x_intial is: "))
+        y = int(input("y_intial is: "))
+        theta = int(int(input("theta_intial is (in degree): "))/30)
+        #x, y, theta = (0, 0, 2)
         ## image coordinates
         row = rows-2*y-1 ; col = 2*x
         if not isValidNode(map_, row, col,0):
@@ -67,6 +72,7 @@ def getStartNode(map_):
             break
     return row, col, theta
 
+#input goal coordinates
 def getGoalNode(map_):
     print("Enter the goal co-ordinates")
     rows, cols= map_.shape[:2]
@@ -95,30 +101,41 @@ class Node:
         self.costCome = costCome
         self.cost = cost
 
-def updateNeighbours(arr, map_, curr_node,queue,goal_node,step_size):
+#function that finds the next node on the path and checks if its feasible
+def updateNeighbours(arr, map_, path_img, curr_node,queue,goal_node,step_size):
     x,y,theta = curr_node
     theta_size = 30
-    
-    for angle in range(0, 12,1):
-        theta_new = (theta+angle)%12
-        x_new = round(x + step_size*math.cos(theta_new*theta_size))
+    #iterate for each of the five directions it can go
+    for angle in range(-2, 3,1):
+        theta_new = (theta+angle)
+        if theta_new >= 12:
+            theta_new -= 12
+        if theta_new < 0:
+            theta_new += 12
+        x_new = round(x + step_size*math.cos(math.radians(theta_new*theta_size)))
         # x_new = round(x_new/threshold)
-        y_new = round(y + step_size*math.sin(theta_new*theta_size))
+        y_new = round(y + step_size*math.sin(math.radians(theta_new*theta_size)))
         # y_new = round(y_new/threshold)
         if isValidNode(map_,x_new,y_new,theta_new, 0):
+            #calculate the cost to come for the new node
             arr[x_new][y_new][theta_new].costCome = arr[x][y][theta].costCome + euclideanDistance((x_new,y_new),(x,y))
+            #calculate the total cost for the new node
             arr[x_new][y_new][theta_new].cost = arr[x_new][y_new][theta_new].costCome + euclideanDistance((x_new,y_new),goal_node)
             #check if visited
             if arr[x_new][y_new][theta_new].visited is False:
+                #mark the node as visited
                 arr[x_new][y_new][theta_new].visited = True
+                #add the node to the queue
                 queue.put((arr[x_new][y_new][theta_new].cost,(x_new,y_new,theta_new)))
                 arr[x_new][y_new][theta_new].parent = (x,y,theta)
-                cv2.line(map_, (y_new,x_new), (y, x), (0, 255, 0), thickness=1, lineType=8)
+                #draw the lines of the path on the map
+                cv2.line(path_img, (y_new,x_new), (y, x), (0, 255, 0), thickness=1, lineType=8)
             else:
+                #if the current node is better than the already visited node
                 if arr[x_new][y_new][theta_new].cost > arr[x][y][theta].costCome + euclideanDistance((x_new,y_new),(x,y)) + euclideanDistance((x_new,y_new),goal_node):
                     queue.put((arr[x_new][y_new][theta_new].cost,(x_new,y_new,theta_new)))
                     arr[x_new][y_new][theta_new].parent = (x,y,theta)
-    return arr, map_
+    return arr, map_, path_img
 
 
 def tracePath(arr,img,curr_node):
@@ -157,11 +174,13 @@ def main():
     map_ = load_map()
     img = map_.copy()
     img = cv2.resize(img,(600,400))
+    path_img = img.copy()
 
     start_node = getStartNode(img)
     # step_size = int(input("step size is: "))
 
-    step_size = 1
+
+    step_size = 5
     goal_node = getGoalNode(img)
     threshold = .5
     step_size = step_size*2
@@ -183,11 +202,12 @@ def main():
         if euclideanDistance(curr_node[:2],goal_node) < 3:
             algo_time = time.time()
             print('found Goal in {}s at cost {}!!'.format(algo_time-start_time, arr[curr_node[0]][curr_node[1]][curr_node[2]].cost))
-            output = tracePath(arr,img,curr_node)
+            output = tracePath(arr,path_img,curr_node)
             break
-        arr, img = updateNeighbours(arr, img, curr_node,queue,goal_node,step_size)
+        arr, img, path_img = updateNeighbours(arr, img, path_img, curr_node,queue,goal_node,step_size)
         #img[curr_node[0]][curr_node[1]] = (0,255,0)
-        #exploredList.append(img)
+        exploredList.append(img)
+        #cv2.imshow('process',path_img)
         key = cv2.waitKey(1) & 0xFF
         if key == 27 or key == ord("q"):
             break
@@ -197,7 +217,7 @@ def main():
     cv2.imwrite('./outputs/Path.jpg',output)
     cv2.imwrite('./outputs/Explored.jpg',img
                 )
-    #saveVideo(exploredList,'./outputs/explored.avi')
+    saveVideo(exploredList,'./outputs/explored.avi')
     cv2.destroyAllWindows()
 
 if __name__=='__main__':