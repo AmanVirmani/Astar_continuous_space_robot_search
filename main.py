6d1d71409b4e4947e22fb12751f6bdd5d2105057
Aman Virmani <aman.virmani26@gmail.com>
2020-03-29 04:19:34 -0400

    added terminal argument parsing

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import time
 import math
 import cv2
 from queue import PriorityQueue
+import argparse
+import imageio
 
 
 # Function to create the map
-def load_map(fname = None):
+def load_map(fname = None,rc=0):
     if fname is not None :
         map_ = cv2.imread(fname)
         return map_
@@ -16,11 +18,11 @@ def load_map(fname = None):
     world = 255*np.ones((200,300,3))
 
     # creating the obstacles
-    obstacle_map.obstacle_circle(world)
-    obstacle_map.obstacle_ellipse(world)
-    obstacle_map.obstacle_rhombus(world)
-    obstacle_map.obstacle_rectangle(world)
-    obstacle_map.obstacle_polygon(world)
+    obstacle_map.obstacle_circle(world,rc)
+    obstacle_map.obstacle_ellipse(world,rc)
+    obstacle_map.obstacle_rhombus(world,rc)
+    obstacle_map.obstacle_rectangle(world,rc)
+    obstacle_map.obstacle_polygon(world,rc)
 
     # save the image of the world
     cv2.imwrite('./outputs/map.jpg',world)
@@ -29,7 +31,7 @@ def load_map(fname = None):
 
 
 # Function to check if a node is valid
-def isValidNode(map_, x, y, theta, r=0):
+def isValidNode(map_, x, y, r=0):
     rows, cols = map_.shape[:2]
 
     # make sure coordinates are within map boundaries
@@ -60,43 +62,22 @@ def euclideanDistance(state1, state2):
 
 
 # Function to take user input start coordinates, theta, radius, and clearance
-def getStartNode(map_,radius):
-    print("Enter the start coordinates")
+def getStartNode(map_, loc, radius):
     rows, cols = map_.shape[:2]
-    while True :
-        # Cartesian Form
-        x = int(input("x_intial is: "))
-        y = int(input("y_intial is: "))
-        theta = int(int(input("theta_intial is (in degree): "))/30)
-        theta -= 3
-        if theta < 0:
-            theta += 12
-        # x, y, theta = (50, 30, 2)
-
-        # converting to image coordinates
-        row = rows-2*y-1 ; col = 2*x
-        if not isValidNode(map_, row, col, theta, radius):
-            print('Input Node not within available map range. Please enter again!')
-        else:
-            break
+    row = rows-2*loc[1]-1; col = 2*loc[0]; theta = int((loc[2]/30)%12)
+    if not isValidNode(map_, row, col, radius):
+        print('Input Node not within available map range. Please enter again!')
+        return None
     return row, col, theta
 
 
 # Function to take user input goal coordinates
-def getGoalNode(map_,radius):
-    print("Enter the goal coordinates")
-    rows, cols= map_.shape[:2]
-    while True:
-        # Cartesian Form
-        x = int(input("x_goal is: "))
-        y = int(input("y_goal is: "))
-        #x, y = 150, 150
-        # image coordinates
-        row = rows-2*y-1 ; col = 2*x
-        if not isValidNode(map_, row, col, 0, radius):
-            print('Input Node not within available map range. Please enter again! ')
-        else:
-            break
+def getGoalNode(map_, loc, radius):
+    rows, cols = map_.shape[:2]
+    row = rows-2*loc[1]-1; col = 2*loc[0]
+    if not isValidNode(map_, row, col, radius):
+        print('Input Node not within available map range. Please enter again!')
+        return None
     return row, col
 
 
@@ -120,14 +101,10 @@ def updateNeighbours(arr, map_, path_img, curr_node, queue, goal_node, step_size
     theta_size = 30
     # iterate for each of the five directions it can go
     for angle in range(-2, 3, 1):
-        theta_new = (theta+angle)
-        if theta_new >= 12:
-            theta_new -= 12
-        if theta_new < 0:
-            theta_new += 12
+        theta_new = (theta+angle)%12
         x_new = round(x + step_size*math.sin(math.radians(theta_new*theta_size)))
         y_new = round(y + step_size*math.cos(math.radians(theta_new*theta_size)))
-        if isValidNode(map_,x_new,y_new,theta_new, radius):
+        if isValidNode(map_,x_new,y_new, radius):
             # calculate the cost to come for the new node
             arr[x_new][y_new][theta_new].costCome = arr[x][y][theta].costCome + euclideanDistance((x_new,y_new),(x,y))
             # calculate the total cost for the new node
@@ -150,34 +127,31 @@ def updateNeighbours(arr, map_, path_img, curr_node, queue, goal_node, step_size
 
 
 def tracePath(arr, img, curr_node):
-    img = np.uint8(img)
     nodes = []
-    output = './outputs/output.avi'
-    h, w = img.shape[:2]
-    out = cv2.VideoWriter(output,cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 1, (w, h))
+    images = []
+    output = './outputs/output.gif'
     while curr_node is not None:
         nodes.append(curr_node)
         curr_node = arr[curr_node[0]][curr_node[1]][curr_node[2]].parent
     nodes = nodes[::-1]
     for i in range(len(nodes)-1):
         cv2.line(img, (nodes[i+1][1],nodes[i+1][0]), (nodes[i][1],nodes[i][0]), (0, 0, 255), thickness=1, lineType=8)
-        out.write(img.copy())
-    out.release()
+        images.append(np.uint8(img.copy()))
+    imageio.mimsave(output, images, fps=55)
 
 
-def main():
-    map_ = load_map('./map.jpg')
+def main(args):
+    rc = args['radius']+args['clearance']
+    map_ = load_map('./map.jpg', rc=rc)
     img = map_.copy()
     threshold = 0.5
     img = cv2.resize(img,(600,400))
     path_img = img.copy()
 
-    r = int(input("Robot's radius is: "))
-    c = int(input("Desired clearance is: "))
-    start_node = getStartNode(img,r+c)
-    step_size = int(input("step size is: "))
+    start_node = getStartNode(img, args['start_node'], rc)
+    step_size = args['step_size']
 
-    goal_node = getGoalNode(img,r+c)
+    goal_node = getGoalNode(img, args['goal_node'], rc)
     step_size = step_size*2
     rows, cols = map_.shape[:2]
 
@@ -188,29 +162,39 @@ def main():
     queue = PriorityQueue()
     queue.put((arr[start_node[0]][start_node[1]][start_node[2]].cost, start_node))
 
-    output = './outputs/explored.avi'
-    h, w = img.shape[:2]
-    out = cv2.VideoWriter(output, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 1, (w, h))
+    exploredList = []
     start_time = time.time()
 
-    path_img = cv2.circle(path_img, (start_node[1], start_node[0]), r+c, (0,0,0), 2)
-    path_img = cv2.circle(path_img, (goal_node[1], goal_node[0]), r+c, (0,0,0), 2)
+    path_img = cv2.circle(path_img, (start_node[1], start_node[0]), rc, (0, 0, 0), 2)
+    path_img = cv2.circle(path_img, (goal_node[1], goal_node[0]), rc, (0, 0, 0), 2)
     while queue:
         curr_node = queue.get()[1]
         if euclideanDistance(curr_node[:2], goal_node) < 3:
             algo_time = time.time()
             print('found Goal in {}s at cost {}!!'.format(algo_time-start_time, arr[curr_node[0]][curr_node[1]][curr_node[2]].cost))
-            tracePath(arr, path_img, curr_node)
+            tracePath(arr, np.uint8(path_img), curr_node)
             break
-        arr, img, path_img = updateNeighbours(arr, img, path_img, curr_node, queue, goal_node, step_size,r+c)
-        out.write(np.uint8(path_img))
+        arr, img, path_img = updateNeighbours(arr, img, path_img, curr_node, queue, goal_node, step_size,rc)
+        exploredList.append(np.uint8(path_img))
         cv2.imshow('process', path_img)
         key = cv2.waitKey(1) & 0xFF
         if key == 27 or key == ord("q"):
             break
-    out.release()
     cv2.destroyAllWindows()
+    imageio.mimsave('./outputs/explored.gif', exploredList, fps=55)
 
 
 if __name__=='__main__':
-    main()
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-r", "--radius", required=False, help="Radius of the robot",
+                    default=1, type=int)
+    ap.add_argument("-c", "--clearance", required=False, help="desired clearance to avoid obstacles",
+                    default=1, type=int)
+    ap.add_argument("-d", "--step_size", required=False, help="step size robot takes in each move",
+                    default=10, type=int)
+    ap.add_argument("-s", "--start_node", required=True, help="Location (x,y) of start node",
+                    nargs='+', type=int)
+    ap.add_argument("-g", "--goal_node", required=True, help="Location (x,y) of goal node",
+                    nargs='+', type=int)
+    args = vars(ap.parse_args())
+    main(args)
